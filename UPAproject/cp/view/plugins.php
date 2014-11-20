16c2b1acae887177da39279999d3c60203b5126d
luisbellido <luis.bellido2010@gmail.com>
2014-11-20 09:05:10 -0500

    Para su Desarrollo

--- UPAproject/cp/view/plugins.php
+++ UPAproject/cp/view/plugins.php
@@ -1,7 +1,7 @@
 <link href="../../js/jqueryeasy/themes/default/easyui.css" rel="stylesheet" type="text/css"/>
 <link href="../../js/jqueryeasy/themes/icon.css" rel="stylesheet" type="text/css"/>
 <link href="../../css/css_modelo.css" rel="stylesheet" type="text/css">
-<link href="../../css/demo.css" rel="stylesheet" type="text/css">
+<link href="../../css/contend.css" rel="stylesheet" type="text/css">
 <script src="../../js/jquery.js"></script>
 <script src="../../js/funcgeneral.js"></script>
 <script src="../../js/jqueryeasy/jquery.easyui.min.js"></script>
